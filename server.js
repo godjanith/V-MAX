e21ae87598a6e485a9188469626b2072fdf63a8d
20252MCA0318JANITH V C <20252MCA0318@presidencyuniversity.in>
2026-08-17 09:51:07 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('This is a Home Page');
+});
+
+
+app.get('/about', (req, res) => {
+    res.send('This is an About Us Page');
+});
+
+
+app.get('/contact', (req, res) => {
+    res.send('This is a Contact Page');
+});
+
+
+app.get('/product', (req, res) => {
+    res.send('This is a Product Page');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
